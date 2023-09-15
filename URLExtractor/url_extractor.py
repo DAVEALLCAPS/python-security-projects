c9772de46220eb34d3e0ec3266f1c7673b99431c
DAVEALLCAPS <daveallcaps@gmail.com>
2023-09-14 21:26:09 -0600

    Added link summary output

--- URLExtractor/url_extractor.py
+++ URLExtractor/url_extractor.py
@@ -1,18 +1,53 @@
 import requests 
 from bs4 import BeautifulSoup 
+from urllib.parse import urlparse
+from collections import Counter
 
 def get_links(url):
     res = requests.get(url)
     soup = BeautifulSoup(res.text, 'lxml')
 
-    links = set()  # Initialize as a set
+    links = set()
     for link in soup.find_all('a', href=True):
         href = link['href']
         if href.startswith('http'):
-            links.add(href)  # Add URL to the set
+            links.add(href)
         else:
-            links.add(url + href)  # Add URL to the set
+            links.add(url + href)
     return links
+
+def summarize_links(links):
+    # Styling constants
+    SEPARATOR = "-" * 75
+    HEADER = "LINK SUMMARY"
+    FOOTER = "=" * 75
+
+    # Total number of links
+    total_links = len(links)
+
+    # HTTP vs HTTPS count
+    http_count = sum(1 for link in links if link.startswith('http://'))
+    https_count = sum(1 for link in links if link.startswith('https://'))
+
+    # Most common domain
+    domains = [urlparse(link).netloc for link in links]
+    most_common_domain = Counter(domains).most_common(1)[0]
+
+    # Unique domains
+    unique_domains = sorted(set(domains))
+    domain_count = len(unique_domains)
+
+    print(SEPARATOR)
+    print(HEADER.center(75))
+    print(SEPARATOR)
+    print(f"Total links: {total_links}")
+    print(f"Protocols - HTTP: {http_count} | HTTPS: {https_count}")
+    print(f"Most common domain: {most_common_domain[0]} (found {most_common_domain[1]} times)")
+    print(f"Unique domains: {domain_count}")
+    print(SEPARATOR)
+    for domain in unique_domains:
+        print(domain)
+    print(FOOTER)
 
 if __name__ == "__main__" : 
     url = input("Enter URL: ")
@@ -25,6 +60,6 @@ if __name__ == "__main__" :
         for link in links:
             f.write(link + '\n')
 
-    print("\nLINKS saved to extracted_links.txt:")
-    for link in links:
-        print(link)
+    print("=" * 75)
+    print(f'Successfully grabbed links from {url} and saved to extracted_links.txt!')
+    summarize_links(links)
